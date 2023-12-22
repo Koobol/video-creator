6b7bfb10613becf65047cec7a9246e0d070794d5
Koobol <koobol0001@gmail.com>
2023-12-22 14:38:00 -0700

    made more complex video

--- test.js
+++ test.js
@@ -13,22 +13,32 @@ export function setup(canvas, { framerate }) {
 }
 
 
-let x = 0;
-let played = false;
+let t = 0;
+let fullRotations = 0;
+let color = "white";
 /** @type Draw */
 export function draw(canvas, { playSound }) {
-  if (x > canvas.width - 100) return 0;
-
-
-  ctx.fillStyle = x <= 200 ? "blue" : "lime";
-  ctx.fillRect(x, (canvas.height - 100) / 2, 100, 100);
-
-
-  if (!played && x > 200) {
+  if (t >= Math.PI * 7) return 0;
+  if (t - fullRotations * Math.PI * 2 > Math.PI * 0.5) {
+    fullRotations += 0.25;
     playSound("beep.wav");
-    played = true;
+
+    color = `#${Math.floor(Math.random() * 0x1000000).toString(16)}`;
   }
 
 
-  x += 200 / fps;
+  ctx.fillStyle = color;
+
+  ctx.beginPath();
+  ctx.arc(
+    Math.sin(t) * 50 + canvas.width / 2,
+    Math.cos(t) * 50 + canvas.height / 2,
+    50,
+    0,
+    2 * Math.PI,
+  );
+  ctx.fill();
+
+
+  t += Math.PI / fps;
 }
