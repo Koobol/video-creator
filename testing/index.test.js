2c46671b3247c012e6a89661d3600663e046b305
Koobol <koobol0001@gmail.com>
2024-03-10 15:11:12 -0600

    added more checks to tests

--- testing/index.test.js
+++ testing/index.test.js
@@ -1,6 +1,6 @@
 // @ts-check
 import VideoCreator from "../src/index";
-import { it, expect, beforeEach, afterEach } from "vitest";
+import { it, expect, beforeEach } from "vitest";
 
 
 customElements.define("video-creator", VideoCreator);
@@ -10,12 +10,11 @@ customElements.define("video-creator", VideoCreator);
 let videoCreator;
 
 beforeEach(() => {
+  document.body.innerHTML = "";
+
   videoCreator = /** @type {VideoCreator} */
     (document.createElement("video-creator"));
   document.body.appendChild(videoCreator);
-});
-afterEach(() => {
-  document.body.innerHTML = "";
 });
 
 
@@ -24,18 +23,45 @@ it("exists", () => {
 });
 it("starts out waiting", () => {
   expect(videoCreator.state).toBe("waiting");
+
+
+  expect(videoCreator.worker).toBe(null);
+  
+
+  expect(videoCreator.playing).toBe(false);
+
+  videoCreator.play();
+  expect(videoCreator.playing).toBe(false);
+
+
+  expect(videoCreator.getFrames()).toBeNull();
+  expect(videoCreator.getAudio()).toBeNull();
 });
 
 it("can be rendered", async () => {
-  const rendered = videoCreator.render(new Worker(
+  const worker = new Worker(
     // @ts-ignore
     new URL("videos/basic.js", import.meta.url),
     { type: "module" },
-  ));
+  )
+  const rendered = videoCreator.render(worker);
+  
+  expect(videoCreator.worker).toBe(worker);
 
 
   expect(videoCreator.state).toBe("rendering");
 
   await rendered;
   expect(videoCreator.state).toBe("rendered");
+
+
+  videoCreator.play();
+  expect(videoCreator.playing).toBe(true);
+
+  videoCreator.pause();
+  expect(videoCreator.playing).toBe(false);
+
+
+  expect(videoCreator.getFrames()?.[0]).toBeInstanceOf(ImageBitmap);
+  expect(videoCreator.getAudio()).toBeInstanceOf(AudioBuffer);
 });
